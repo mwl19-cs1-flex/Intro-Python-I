fcf103b8d0de1074903e27f0e2f104626826f0a7
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-03-09 09:38:45 -0700

    Finished 00 Hello World

--- src/00_hello.py
+++ src/00_hello.py
@@ -1,1 +1,16 @@
 # Print "Hello, world!" to your terminal
+
+## Understand
+# I want to print the phrase "Hello World" to the terminal
+# I will need a function that will print to the terminal
+
+## Plan
+# Research functions that print to the terminal
+# A function exists that prints to the terminal, called print
+
+## Execute
+# print(Hello World) didn't work, why?
+print("Hello World!") # This works!
+
+## Reflect
+# Syntax error, lets look at our spec
