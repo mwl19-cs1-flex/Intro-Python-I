6c2753f7389fdab6d44c9948cd7d957a7dacfe37
MWeberLambdaweb19 <mack.webb37@gmail.com>
2020-03-09 09:46:28 -0700

    Finished 01 bignum

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -1,4 +1,18 @@
 # Print out 2 to the 65536 power
 # (try doing the same thing in the JS console and see what it outputs)
 
-# YOUR CODE HERE
+## JavaScript
+# 2**65536 returns Infinity
+
+## Understand
+# Print (means use the print() function) 2 to the 65536th power
+
+## Plan 
+# Research how to do exponential operators in Python
+# It is the same way in JS as it is in Py
+
+## Execute
+print(2**65536)
+
+## Reflect
+# It works! Is there a faster/better way?
